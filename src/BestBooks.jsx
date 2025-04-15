114d6d52121c5f9f9b81e4c0f663977567d14889
Jason <jtimm@gicw.org>
2025-04-15 15:34:47 -0700

    completed lab11

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,30 +1,73 @@
 import React from 'react';
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import bookImg from './book.png';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
-    }
+      books: [],
+      errorMessage: ''
+    };
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount = async () => {
+    try {
+      const config = {
+        method: 'get',
+        baseURL: import.meta.env.VITE_SERVER_URL, // ✅ Make sure your .env uses this
+        url: '/books'
+      };
+      const response = await axios(config);
+      this.setState({
+        books: response.data,
+        errorMessage: ''
+      });
+    } catch (error) {
+      console.error('Error in componentDidMount', error);
+      this.setState({
+        errorMessage: `statusCode ${error.response?.status}: ${error.response?.data || 'Something went wrong'}`
+      });
+    }
+  };
+
+  closeError = () => {
+    this.setState({ errorMessage: '' });
+  };
 
   render() {
-
-    /* TODO: render all the books in a Carousel */
-
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+        <h2>My Essential Lifelong Learning & Formation Shelf</h2>
+        <Container>
+          {this.state.books.length ? (
+            <Carousel id="carousel">
+              {this.state.books.map(book => (
+                <Carousel.Item key={book._id}>
+                  <Image
+                    id="Images"
+                    className="carousel-img"
+                    src={bookImg}
+                    alt={book.title}
+                    fluid
+                  />
+                  <Carousel.Caption id="carousel-text-box">
+                    <h2 className="carousel-text">{book.title}</h2>
+                    <p className="carousel-text">{book.description}</p>
+                    <p className="carousel-text">Status: {book.status}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          ) : (
+            <h3>No Books Found :</h3>
+          )}
+        </Container>
       </>
-    )
+    );
   }
 }
 
