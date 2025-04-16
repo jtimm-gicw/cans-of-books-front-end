cefc3d7fdc9d75a5a6ba83856b01773e7cd18498
Jason <jtimm@gicw.org>
2025-04-16 11:15:38 -0700

    added about page info

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,7 @@ class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return <p>I'm Jaosn. I am an educator and software developer.  I love reading books and I especially enjoy audiobooks.  My favorite genre of books are Fantasy and Sci-fi however I enjoy all types of books.  I hope you are entertained by my selections. </p>
   }
 }
 
